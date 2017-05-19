34debbdc144437ff2ab267ffdf87b98174ff8cc0
drangob <drangob123@gmail.com>
2017-05-19 17:16:45 +0100

    Fixed the setup in terms of header files

--- person.cpp
+++ person.cpp
@@ -5,51 +5,48 @@
 
 using namespace std;
 
-class Person {
-	std::string preNominal, name, postNominal;
-	bool isMale;
-	double age;
-	int maxAge;
-public:
-	Person(std::string name, double age, bool isMale, int maxAge){
-		cout << "Object is being created " << endl;
-		this->name.assign(name);
-		this->isMale = isMale;
-		if (isMale){
-			preNominal = "Mr";
-		} else {
-			preNominal = "Miss";
-		}
-		this->maxAge = maxAge;
-	}
-	std::string getName(){
-		return preNominal + " " + name + " " + postNominal;
-	}
-	int ageUp(){
-		this->age += .25;
-		srand(time(NULL));
-		if(this->age > 60) {
-			//chance is 1 in DEATHAGE - age
-			//110 - 60 = 50, 1 in 50 chance
-			int chance = (maxAge - this->age);
-			if ((rand() % chance) <= 1) {
-				//die
-				this->die();
-				return 1;
-			}
-		}
-		return 0;
-	}
-	int die(){
-		//die
-		this->printAge();
-		cout << "die!" << endl;
-	}
 
-	int printAge(){
-		cout << "Age = " << this->age << endl;
-	}	
-};
+Person::Person(std::string name, double age, bool isMale, int maxAge){
+	cout << "Object is being created " << endl;
+	this->name.assign(name);
+	this->isMale = isMale;
+	if (isMale){
+		preNominal = "Mr";
+	} else {
+		preNominal = "Miss";
+	}
+	this->age = age;
+	this->maxAge = maxAge;
+}
+std::string Person::getName(){
+	return preNominal + " " + name + " " + postNominal;
+}
+int Person::ageUp(){
+	this->age += .25;
+	srand(time(NULL));
+	if(this->age > 60) {
+		//chance is 1 in DEATHAGE - age
+		//110 - 60 = 50, 1 in 50 chance
+		int chance = (maxAge - this->age);
+		if ((rand() % chance) <= 1) {
+			//die
+			this->die();
+			return 1;
+		}
+	}
+	return 0;
+}
+
+int Person::die(){
+	//die
+	this->printAge();
+	cout << "die!" << endl;
+}
+
+int Person::printAge(){
+	cout << "Age = " << this->age << endl;
+}
+
 
 
 
